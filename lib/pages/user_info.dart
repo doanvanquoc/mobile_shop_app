3f2c1eae2756176ea46947264f7ff4bb4741b8fc
Lehuuthanh23 <thanhlebtre1245@gmail.com>
2023-10-13 01:24:33 +0700

    Thêm drawer

--- lib/pages/user_info.dart
+++ lib/pages/user_info.dart
@@ -10,6 +10,45 @@ class UserInfoPage extends StatelessWidget {
         centerTitle: true,
         title: const Text('Thông Tin Cá Nhân'),
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+                child: Container(
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(10)),
+              child: const ListTile(
+                leading: CircleAvatar(
+                  child: Text('Q'),
+                ),
+                title: Text('Đoàn Văn Quốc'),
+                subtitle: Text('Hạng Kim Cương'),
+              ),
+            )),
+            const ListTile(
+              title: Text(
+                'Trang chủ',
+                style: TextStyle(color: Colors.grey),
+              ),
+              leading: Icon(Icons.account_circle),
+            ),
+            const ListTile(
+              title: Text(
+                'Cài đặt',
+                style: TextStyle(color: Colors.grey),
+              ),
+              leading: Icon(Icons.settings),
+            ),
+            const ListTile(
+              title: Text(
+                'Đăng xuất',
+                style: TextStyle(color: Colors.grey),
+              ),
+              leading: Icon(Icons.logout),
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Trang chủ'),
