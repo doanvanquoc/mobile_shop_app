9c97948875d28332b4f788fadcba032bde695eaf
Đoàn Văn Quốc <vanquoc.dev@gmail.com>
2023-10-12 19:44:23 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_shop_app/pages/user_info.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Mobile Shop App',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: const UserInfoPage(),
+    );
+  }
+}
