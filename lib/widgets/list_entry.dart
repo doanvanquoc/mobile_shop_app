9bf96aea099806a99a3fb773aeb5bd114119a47f
fut <phucnguyen1922003@gmail.com>
2023-10-19 18:27:33 +0700

    complete list_entry.dart

--- lib/widgets/list_entry.dart
+++ lib/widgets/list_entry.dart
@@ -10,35 +10,55 @@ class EntryScreen extends StatefulWidget {
 }
 
 List<Entry> viewEntry = [
-  Entry(img: "assets/img/durian.png", title: "title", desc: "desc"),
-  Entry(img: "assets/img/durian.png", title: "title", desc: "desc"),
-  Entry(img: "assets/img/durian.png", title: "title", desc: "desc"),
-  Entry(img: "assets/img/durian.png", title: "title", desc: "desc"),
-  Entry(img: "assets/img/durian.png", title: "title", desc: "desc"),
-  Entry(img: "assets/img/durian.png", title: "title", desc: "desc"),
-  Entry(img: "assets/img/durian.png", title: "title", desc: "desc"),
+  Entry(img: "assets/img/durian.png", title: "Tiêu đề nè", desc: "Mô tả nè"),
+  Entry(img: "assets/img/durian.png", title: "Tiêu đề nè", desc: "Mô tả nè"),
+  Entry(img: "assets/img/durian.png", title: "Tiêu đề nè", desc: "Mô tả nè"),
+  Entry(img: "assets/img/durian.png", title: "Tiêu đề nè", desc: "Mô tả nè"),
+  Entry(img: "assets/img/durian.png", title: "Tiêu đề nè", desc: "Mô tả nè"),
+  Entry(img: "assets/img/durian.png", title: "Tiêu đề nè", desc: "Mô tả nè"),
+  Entry(img: "assets/img/durian.png", title: "Tiêu đề nè", desc: "Mô tả nè"),
 ];
 
 class _EntryScreenState extends State<EntryScreen> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: SizedBox(
-        height: 150,
-        child: ListView.builder(
-            scrollDirection: Axis.vertical,
-            itemCount: viewEntry.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 232, 229, 222),
+      body: ListView.builder(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 35),
+          scrollDirection: Axis.vertical,
+          itemCount: viewEntry.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 7),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 1,
+                      blurRadius: 4,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(10),
+                ),
                 child: ListTile(
-                  leading: Image.network(viewEntry[index].img),
-                  title: Text(viewEntry[index].title),
+                  leading: Image.asset(viewEntry[index].img),
+                  title: Text(
+                    viewEntry[index].title,
+                    style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                   subtitle: Text(viewEntry[index].desc),
                 ),
-              );
-            }),
-      ),
+              ),
+            );
+          }),
     );
   }
 }
