691382caa97f8c2a7e52d5a98763a6302bc8c19f
fut <phucnguyen1922003@gmail.com>
2023-10-19 16:21:40 +0700

    create class entry

---
+++ lib/widgets/list_entry.dart
@@ -0,0 +1,10 @@
+import '../models/entry.dart';
+
+List<Entry> viewEntry = [
+  Entry(img: "", title: "", desc: ""),
+  Entry(img: "", title: "", desc: ""),
+  Entry(img: "", title: "", desc: ""),
+  Entry(img: "", title: "", desc: ""),
+  Entry(img: "", title: "", desc: ""),
+  Entry(img: "", title: "", desc: ""),
+];
