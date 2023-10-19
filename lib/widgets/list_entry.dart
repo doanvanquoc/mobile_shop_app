f855e4475bb79b040fba89b2f3a678facc577c4f
Lehuuthanh23 <thanhlebtre1245@gmail.com>
2023-10-19 16:24:48 +0700

    Thanh011

---
+++ lib/widgets/list_entry.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ListEntry extends StatelessWidget {
+  const ListEntry({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.amber[100],
+      ),
+      child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: ListTile(
+            title: const Text("Tiêu đề"),
+            subtitle: const Text("Tóm tắt 01"),
+            leading: ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(50)),
+              child: Image.asset(
+                "assets/img/avatar.jpg",
+                //fit: BoxFit.cover,
+              ),
+            ),
+            
+          )),
+    );
+  }
+}
